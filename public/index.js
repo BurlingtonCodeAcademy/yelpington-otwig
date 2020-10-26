66e21619e943134121c54993b11b3b4afbea8f6c
ohartwig <45525308+otwig@users.noreply.github.com>
2020-10-26 08:19:25 -0400

    added css and worked on further stories

--- public/index.js
+++ public/index.js
@@ -1,21 +1,32 @@
-fetch('./api')
+// below fetch populates the nav bar next to the map in index.html
+//fetch is called to gather info from our individual files within the API folder
+fetch('./restaurantapi/restaurant-index')
+	//for every file found 
 	.then(res => res.json())
+	//iterate over and do the following
 	.then(restIndexArray => {
+		//for each restaurant:
 		restIndexArray.forEach((restaurant) => {
+			//create a link tag
 			let anchor = document.createElement(`a`)
-			anchor.href = `./api/${restaurant}`
+			//set the path with the link tag to:
+			anchor.href = `./restaurant/${restaurant}`
+			//then create a variable called newRest and
+			//remove the dashes and capitalize every word in the restaurant id
 			let newRest = restaurant.split("-").map(toTitleCase).join(' ')
-			let anchorInner = toTitleCase(newRest)
-			anchor.innerHTML = anchorInner
+			anchor.innerHTML = newRest
+			//and insert the link tag to the nav div within index.html
 			document.getElementById('mapNav').appendChild(anchor)
 		})
 	})
 
+// this function capitalizes every word within the restaurant id
 function toTitleCase(string) {
 	return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~MAP INFO BELOW~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 //Coordinates for where our map opens
 let myMap = L.map('mapid').setView([40.681162, -73.96981], 16)
 
@@ -25,7 +36,7 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.pn
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(myMap)
 
-//Map marker
+//Map marker assigned to latitude and longitude locations
 let amplehills = L.marker([40.678710, -73.968200]).addTo(myMap)
 let bergenbagels = L.marker([40.681020, -73.974790]).addTo(myMap)
 let bklynlarder = L.marker([40.680520, -73.975270]).addTo(myMap)
@@ -33,7 +44,7 @@ let look = L.marker([40.679900, -73.964450]).addTo(myMap)
 let mrmelon = L.marker([40.682911, -73.964169]).addTo(myMap)
 let zaytoons = L.marker([40.678688, -73.968474]).addTo(myMap)
 
-//Map marker alert pop-up
+//Map marker alert pop-up when clicked
 amplehills.bindPopup('amplehill')
 bergenbagels.bindPopup('bergenbagels')
 bklynlarder.bindPopup('bklynlarder')
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express()
 
 //variables
 const path = require('path')
-const restDir = path.resolve('./api')
 
 //middleware
 app.use(express.static('public'))
@@ -19,42 +18,27 @@ app.get('/', (req, res) => {
 })
 
 //display all restaurant entries as json objects
-app.get('/api', (req, res) => {
+app.get('/restaurantapi', (req, res) => {
     res.sendFile(path.resolve('./api/restaurant-index.json'))
 })
 
 //display the restaurant entry that was called (in json format)
-app.get('/api/:id', (req, res) => {
+app.get('/restaurantapi/:id', (req, res) => {
     let id = req.params.id
     res.sendFile(path.resolve(`./api/${id}.json`))
 })
 
 //display the restaurant entry that was called in text
-app.get('/api/text/:address', (req, res) => {
-    let allData = req.params
-    let data = JSON.stringify(allData)
-    res.type('text/json')
-    res.sendFile(path.resolve(`./api/text/${params}`))
+app.get('/restaurant/:restId', (req, res) => {
+    let restId = req.params.restId
+    res.sendFile(path.resolve(`./public/rest.html/`))
 })
 
-//display the restaurant entry that was called (in HTML)
-app.get('/api/:restId', (req, res) => {
-    const restId = req.params.restId
-    let filePath = restFilePath(restId)
-    if (fs.existsSync(filePath)) {
-        let htmlFile = path.resolve("./public/restaurant.html")
-        res.sendFile(htmlFile)
-    } else {
-        res.sendStatus(404, `Article not found`)
-    }
-   // res.sendFile(path.resolve(`api/${restaurant}.html`))
-  
-})
 
 //set app to listen for port or default to localhost and report back which the app is running on
 const server = app.listen(process.env.PORT || 8000, () => console.log('App is running on port: ', server.address().port))
 
-// helper functions
-function restFilePath(restId) {
-    return path.join(restDir, restId + '.json')
-}
+// // helper functions
+// function restFilePath(restId) {
+//     return path.join(restDir, restId + '.json')
+// }
